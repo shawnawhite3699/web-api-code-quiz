2303f90266237fcf08bd2b5ba28fcbf12dcbbd84
Shawna White <sw1056@gmail.com>
2022-10-04 16:11:23 -0500

    start button setup, HTML structure for questions, JS question array

---
+++ script.js
@@ -0,0 +1,55 @@
+var startBtn = document.getElementById("startBtn");
+var time = 75;
+
+var questions = [
+    {
+        prompt: "Commonly used data types DO NOT include:",
+        options: [
+            "1. strings",
+            "2. booleans",
+            "3. alerts",
+            "4. numbers"
+        ],
+        correctResponse: 2 
+    },
+    {
+        prompt: "The condition in an if/else statement is enclosed within ______.",
+        options: [
+            "1. quotes",
+            "2. curly brackets",
+            "3. parentheses",
+            "4. square brackets"
+        ],
+        correctResponse: 2 
+    },
+    {
+        prompt: "Arrays in JavaScript can be used to store ______.",
+        options: [
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above"
+        ],
+        correctResponse: 3 
+    },
+    {
+        prompt: "String values must be enclosed within _____ when being assigned to variables.",
+        options: [
+            "1. commas",
+            "2. curly brackets",
+            "3. quotes",
+            "4. parentheses"
+        ],
+        correctResponse: 2 
+    },
+    {
+        prompt: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console log"
+        ],
+        correctResponse: 3 
+    },
+];
